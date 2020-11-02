7396f4791c465db4cb9e488a8e7a51ea2e778a94
Maikki Heijala <maikki.heijala@live.com>
2020-11-02 15:56:24 +0200

    Unicafe-projektin Maksukortin testit

--- laskarit/viikko2/Unicafe/src/test/java/com/mycompany/unicafe/MaksukorttiTest.java
+++ laskarit/viikko2/Unicafe/src/test/java/com/mycompany/unicafe/MaksukorttiTest.java
@@ -17,4 +17,39 @@ public class MaksukorttiTest {
     public void luotuKorttiOlemassa() {
         assertTrue(kortti!=null);      
     }
+    
+    @Test
+    public void konstruktoriAsettaaSaldonOikein() {
+        assertEquals("saldo: 0.10", kortti.toString());
+    }
+    
+    @Test
+    public void kortilleVoiLadataRahaa() {
+        kortti.lataaRahaa(90);
+        assertEquals("saldo: 1.0", kortti.toString());
+    }
+    
+    @Test
+    public void saldoVaheneeKunOnTarpeeksiRahaa() {
+        kortti.lataaRahaa(90);
+        kortti.otaRahaa(40);
+        assertEquals("saldo: 0.60", kortti.toString());
+    }
+    
+    @Test
+    public void saldoEiVaheneKunEiOleTarpeeksiRahaa() {
+        kortti.otaRahaa(40);
+        assertEquals("saldo: 0.10", kortti.toString());
+    }
+    
+    @Test
+    public void palauttaaTrueKunRahatRiittavat() {
+        assertEquals(true, kortti.otaRahaa(5));
+    }
+    
+    @Test
+    public void palauttaaFalseKunRahatEivatRiita() {
+        assertEquals(false, kortti.otaRahaa(15));
+    }
+    
 }
