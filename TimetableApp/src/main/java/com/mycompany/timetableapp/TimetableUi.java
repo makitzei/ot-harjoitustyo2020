8802366e78616b82fa6fb99bbb515edb67542d9e
Maikki Heijala <maikki.heijala@live.com>
2020-11-11 14:32:04 +0200

    Mainin ja UI:n alustusta

---
+++ TimetableApp/src/main/java/com/mycompany/timetableapp/TimetableUi.java
@@ -0,0 +1,18 @@
+package com.mycompany.timetableapp;
+
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+public class TimetableUi extends Application {
+    
+    @Override
+    public void start(Stage ikkuna) {
+        ikkuna.setTitle("Hei Lukujärjestyssovellus!");
+        ikkuna.show();
+    }
+
+    public static void main(String[] args) {
+        launch(TimetableUi.class);
+    }
+    
+}
